c8c202445ae5284a584fe2bc3e2027c54b5c6b1f
avbutlov <butloff666@gmail.com>
2021-03-07 01:28:38 +0300

    feat: create socket connection

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server, { cors: { origin: "*" } });
+const http = require("http").Server(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(req.body);
     res.send('dwdw')
 })
 
-server.listen(7777, () => {
+io.on('connection', socket => {
+    console.log(socket.id)
+})
+
+http.listen(9999, () => {
     console.log('Server is running...')
 })
