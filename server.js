aa1e58f1845f908d22def88fe3e1a2dcdfcd2ca4
avbutlov <butloff666@gmail.com>
2021-03-07 14:29:39 +0300

    rebuild

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
