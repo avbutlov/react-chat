e0b480a4d64dfec398a9f0c26760d3b65a8897ac
avbutlov <butloff666@gmail.com>
2021-03-07 13:22:03 +0300

    feat: get request for right access to routes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')))
 
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const rooms = {};
 
 io.on("connection", (socket) => {
